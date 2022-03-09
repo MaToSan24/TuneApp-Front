979565ad0814ff13afcad84c46b148cffd860ad6
Mariano Manuel Torrado Sánchez <marianomtorrados@gmail.com>
2022-03-09 14:11:39 +0100

    Test fixed

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -5,15 +5,12 @@ const expect = require('chai').expect;
 chai.use(chaiHttp);
 const url = 'http://localhost:3000/api/v1';
 
-let userId;
-
 describe('Register: ', () => {
     it('should register successfully', (done) => {
     chai.request(url)
       .post('/auth/register')
-      .send({username: "testUsername", password: "testPassword"})
+      .send({username: "testUsername", password: "testPassword", isTestUser: true})
       .end(function(err,res) {
-        userId = res.body.userId;
         expect(res).to.have.status(200);
         done();
       });
@@ -35,7 +32,7 @@ describe('Login: ', () => {
 describe('Delete user: ', () => {
   it('should delete the users created on the previous tests', (done) => {
     chai.request(url)
-    .delete('/users/' + userId)
+    .delete('/users/test')
     .end(function(err,res) {
       expect(res).to.have.status(200);
       done();
